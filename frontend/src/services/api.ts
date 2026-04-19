12869ddad221cf27a10749afc7625670de97eea5
willianbife <willianbarreirar@gmail.com>
2026-04-19 14:54:07 -0300

    correção deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('@ApiMonitor:token');
+  const token = localStorage.getItem("@ApiMonitor:token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
